9aef1b7b1ca7a0ae143a66bce9d6c11de9a318d0
ArtinRezaee <artin.anzabee@gmail.com>
2023-04-26 20:44:13 -0600

    fix import

--- src/client/generated/hooks.ts
+++ src/client/generated/hooks.ts
@@ -1,64 +1,63 @@
-import * as Types from './operations';
-
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
+import * as Types from './operations';
 const defaultOptions = {} as const;
 
 export const GetWebsiteDataDocument = gql`
-    query GetWebsiteData {
-  navbarCollection(limit: 10, order: order_ASC) {
-    items {
-      titlle
-      linkToExternal
-      order
-      link
-    }
-  }
-  about(id: "1Qac60YjPkBCePrWEMXn2O") {
-    sectionName
-    description {
-      json
-    }
-    image {
-      url
-    }
-  }
-  experienceEntryCollection(limit: 10, order: startDate_DESC) {
-    items {
-      dateRange
-      role
-      roleHeader
-      roleDescription
-      company
-      isPresent
-      startDate
-      endDate
-      logo {
-        url
+  query GetWebsiteData {
+    navbarCollection(limit: 10, order: order_ASC) {
+      items {
+        titlle
+        linkToExternal
+        order
+        link
       }
     }
-  }
-  contactLinksCollection(order: sys_firstPublishedAt_ASC) {
-    items {
-      title
-      description
-      buttonText
-      link
-      type
-    }
-  }
-  projectsCollection(limit: 10, order: sys_firstPublishedAt_ASC) {
-    items {
-      name
+    about(id: "1Qac60YjPkBCePrWEMXn2O") {
+      sectionName
+      description {
+        json
+      }
       image {
         url
       }
-      description
-      link
+    }
+    experienceEntryCollection(limit: 10, order: startDate_DESC) {
+      items {
+        dateRange
+        role
+        roleHeader
+        roleDescription
+        company
+        isPresent
+        startDate
+        endDate
+        logo {
+          url
+        }
+      }
+    }
+    contactLinksCollection(order: sys_firstPublishedAt_ASC) {
+      items {
+        title
+        description
+        buttonText
+        link
+        type
+      }
+    }
+    projectsCollection(limit: 10, order: sys_firstPublishedAt_ASC) {
+      items {
+        name
+        image {
+          url
+        }
+        description
+        link
+      }
     }
   }
-}
-    `;
+`;
 
 /**
  * __useGetWebsiteDataQuery__
@@ -75,14 +74,37 @@ export const GetWebsiteDataDocument = gql`
  *   },
  * });
  */
-export function useGetWebsiteDataQuery(baseOptions?: Apollo.QueryHookOptions<Types.GetWebsiteDataQuery, Types.GetWebsiteDataQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<Types.GetWebsiteDataQuery, Types.GetWebsiteDataQueryVariables>(GetWebsiteDataDocument, options);
-      }
-export function useGetWebsiteDataLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<Types.GetWebsiteDataQuery, Types.GetWebsiteDataQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<Types.GetWebsiteDataQuery, Types.GetWebsiteDataQueryVariables>(GetWebsiteDataDocument, options);
-        }
-export type GetWebsiteDataQueryHookResult = ReturnType<typeof useGetWebsiteDataQuery>;
-export type GetWebsiteDataLazyQueryHookResult = ReturnType<typeof useGetWebsiteDataLazyQuery>;
-export type GetWebsiteDataQueryResult = Apollo.QueryResult<Types.GetWebsiteDataQuery, Types.GetWebsiteDataQueryVariables>;
+export function useGetWebsiteDataQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    Types.GetWebsiteDataQuery,
+    Types.GetWebsiteDataQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<
+    Types.GetWebsiteDataQuery,
+    Types.GetWebsiteDataQueryVariables
+  >(GetWebsiteDataDocument, options);
+}
+export function useGetWebsiteDataLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    Types.GetWebsiteDataQuery,
+    Types.GetWebsiteDataQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<
+    Types.GetWebsiteDataQuery,
+    Types.GetWebsiteDataQueryVariables
+  >(GetWebsiteDataDocument, options);
+}
+export type GetWebsiteDataQueryHookResult = ReturnType<
+  typeof useGetWebsiteDataQuery
+>;
+export type GetWebsiteDataLazyQueryHookResult = ReturnType<
+  typeof useGetWebsiteDataLazyQuery
+>;
+export type GetWebsiteDataQueryResult = Apollo.QueryResult<
+  Types.GetWebsiteDataQuery,
+  Types.GetWebsiteDataQueryVariables
+>;
